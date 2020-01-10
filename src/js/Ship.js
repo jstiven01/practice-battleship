52fcf009362ab9868770b00d6e0e546474c65bb7
jstiven01 <stiven10@gmail.com>
2020-01-10 13:16:12 -0500

    TDD creating factory function Ship

--- src/js/Ship.js
+++ src/js/Ship.js
@@ -1,4 +1,18 @@
-const Ship = () => {
+const Ship = (length) => {
+    const shipArray = Array(length).fill(false);
+
+    const hit = (position) => {
+        if(!shipArray[position]) shipArray[position] = true;
+    }
+
+    const isSunk = () => {
+        return shipArray.every(position => position === true);
+    }
+
+    return {
+        hit, isSunk
+    }
+
 
 };
 export default Ship;
--- test/ship.test.js
+++ test/ship.test.js
@@ -1,3 +1,17 @@
 import Ship from '../src/js/Ship'
 
-test.todo('Testing setup');
+test('hitting a ship in one position and checking is NOT sunk', ()=> {
+    const newShip = Ship(3);
+    newShip.hit(1);
+    expect(newShip.isSunk()).toBe(false);
+
+});
+
+test('hitting a ship in all positions and checking is Sunk', ()=> {
+    const newShip = Ship(3);
+    newShip.hit(0);
+    newShip.hit(1);
+    newShip.hit(2);
+    expect(newShip.isSunk()).toBe(true);
+});
+
