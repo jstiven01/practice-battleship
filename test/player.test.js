82a685370b97a0b50f044e8daa3e1bdfaaf89a57
jstiven01 <stiven10@gmail.com>
2020-01-11 15:46:34 -0500

    Creating Unit test cases for Player

---
+++ test/player.test.js
@@ -0,0 +1,9 @@
+import Player from '../src/js/Player'
+
+test.todo('creating Player human');
+test.todo('creating Player Computer');
+test.todo('Player placing a ship')
+test.todo('Player Human make an missed attack and change turn');
+test.todo('Player Human make an attack hitting a ship and keeping turn');
+test.todo('Player Computer make an missed attack and change turn');
+test.todo('Player Computer make an attack hitting a ship and keeping turn');
