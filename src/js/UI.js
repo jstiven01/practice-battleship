3a8a6fcc5dc97aa9eb29a1551425256c6345bb70
jstiven01 <stiven10@gmail.com>
2020-01-13 19:56:51 -0500

    Adding renderAttackMovement and styling

---
+++ src/js/UI.js
@@ -0,0 +1,60 @@
+const UI = (()=>{
+
+    const boardPlayer1 = document.getElementById('board-player-1');
+    const boardPlayer2 = document.getElementById('board-player-2');
+
+    const renderInitialBoards = (player, gameBoard)=> {
+
+        let divRender;
+        console.log(player.type)
+        if(player.type === 'H'){
+            divRender = boardPlayer1
+        } else {
+            divRender = boardPlayer2;
+        }
+        divRender.innerHTML = '';
+        for (let i = 0; i < gameBoard.getBoard().length; i+=1) {
+            let divGroup =  document.createElement('div');
+            divGroup.setAttribute('class','btn-group size-board');
+            for (let j = 0; j < gameBoard.getBoard().length; j+=1) {
+                let button = document.createElement('button');
+                button.setAttribute('class','m-1 size-button');
+                button.setAttribute('id', `btn${i}${j}`);
+                if(player.type === 'H') button.classList.add(`color-button-${gameBoard.getBoard()[i][j]}`);
+                if(player.type === 'C') {
+                  button.classList.add('button-position-player2');
+                  button.setAttribute('data-row', `${i}`);
+                  button.setAttribute('data-column', `${j}`);    
+                }
+                divGroup.appendChild(button);
+            }
+            divRender.appendChild(divGroup);
+        }
+
+    }
+
+    const renderAttackBoard = (player, gameBoard, position = {}, e = null) => {
+
+        
+        if(player.type === 'H'){
+            renderInitialBoards(player, gameBoard);
+        } else {
+            let button = document.getElementById(`${e.target.id}`)
+            e.target.classList.add(`color-button-${gameBoard.getBoard()[position.row][position.column]}`)
+        }
+    }
+
+    const disablePlayer = ()=> {
+        const boardPlayer2 = document.getElementById('board-player-2');
+        boardPlayer2.classList.toggle('disable-player');
+
+    }
+
+
+    return {
+        renderInitialBoards, disablePlayer, renderAttackBoard,
+    }
+
+})();
+
+export default UI
